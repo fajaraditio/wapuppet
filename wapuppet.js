11444e528e48476ec622fb354be0ceceacd969bd
Aditio <faditiokur@gmail.com>
2020-12-19 17:01:59 +0700

    ⚡️ Add express validator

--- wapuppet.js
+++ wapuppet.js
@@ -4,8 +4,10 @@ const { Client } = require('whatsapp-web.js')
 const fs = require('fs');
 const qrcode = require('qrcode')
 const express = require('express')
+const { body, validationResult } = require('express-validator');
 const http = require('http')
 const socketIO = require('socket.io')
+const basicAuth = require('express-basic-auth')
 
 const app = express()
 const port = 8989
@@ -14,6 +16,15 @@ const io = socketIO(server)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+const Auth = (username, password) => {
+    const matchUser = basicAuth.safeCompare(username, process.env.CLIENT_KEY)
+    const matchPass = basicAuth.safeCompare(password, process.env.CLIENT_SECRET)
+
+    return matchUser & matchPass
+}
+
+app.use(basicAuth({ authorizer: Auth, challenge: true }))
 
 const SESSION_FILE_PATH = './session.json'
 
@@ -47,6 +58,21 @@ const client = new Client({
 })
 
 client.initialize();
+
+// Functions and Helpers
+
+const formattingPhoneNumber = (number, prefix = 62) => {
+    let giveNumberOnly = number.replace(/\D/g, '')
+
+    if (giveNumberOnly.startsWith('0')) {
+        formatted = prefix + giveNumberOnly.substr(1)
+    }
+    else {
+        formatted = giveNumberOnly
+    }
+
+    return formatted
+}
 
 // Initialize Socket.IO
 
@@ -95,19 +121,43 @@ io.on('connection', (socket) => {
     });
 });
 
-app.post('/', (req, res) => {
+app.post('/', [
+    body('phoneNumber').notEmpty(),
+    body('message').notEmpty(),
+], (req, res) => {
+
+    const errors = validationResult(req).formatWith(({ msg }) => { return msg; });
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            status: 'error',
+            code: 400,
+            message: errors,
+        })
+    }
+
     let phoneNumber = req.body.phone_number
     let message = req.body.message
 
     client.isRegisteredUser(`${phoneNumber}@c.us`).then(isRegistered => {
         if (isRegistered) {
-            client.sendMessage(`${phoneNumber}@c.us`, message)
+            client.sendMessage(`${phoneNumber}@c.us`, message).then(response => {
+                res.status(200).json({
+                    status: 'success',
+                    code: 200,
+                    message: 'Message successfully sent',
+                })
+            })
+        }
+        else {
+            res.status(400).json({
+                status: 'success',
+                code: 400,
+                message: 'Message failed to send',
+            })
         }
     })
 
-    res.json({
-        status: 'success'
-    })
 })
 
 server.listen(port, () => {
